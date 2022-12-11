44b7106502a80d27b4ddabc9cbdd0a4a6f83a62b
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2022-12-11 13:41:29 -0300

    first commit

---
+++ primeiro-back-end/src/main/java/com/projetobackend/primeirobackend/PrimeiroBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.projetobackend.primeirobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroBackEndApplication.class, args);
+	}
+
+}
---
+++ primeiro-back-end/src/test/java/com/projetobackend/primeirobackend/PrimeiroBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetobackend.primeirobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
